41d8abbd890fdfc1c716a01bf5a0c6b447868106
Pat Conley <pconley312@gmail.com>
2018-01-13 21:06:41 -0500

    add commands

--- exec.sh
+++ exec.sh
@@ -1,6 +1,10 @@
 docker stop $(docker ps -a -q)
 docker rm   $(docker ps -a -q)
 docker build -t hero-api-server .
-docker run -dit -p 8080:80 --name hero-api-server-run hero-api-server
-
+//-- run the server in interactive mode
 docker run --rm -it -v "$PWD:/usr/src/app" -p 3000:3000 --name app hero-api-server bash
+//-- may need to initialize the db
+rake db:migrate
+rake db:add_heroes
+//-- then start the server
+rails server
