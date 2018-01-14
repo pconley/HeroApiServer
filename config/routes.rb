c980ea4ce233f368b292f136fda98a7bc3fe484d
Pat Conley <pconley312@gmail.com>
2018-01-14 10:59:41 -0500

    add more options

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :heros
   match '/heros' => "heros#options", via: :options
+  match '/heros/:id' => "heros#options", via: :options
   match '/heros' => "heros#update", via: :put
 end
